82f33cfa50915d8eb43881dae4a5fc8cfd0a2029
adamnguyen7 <adangu814@gmail.com>
2022-02-20 12:32:04 -0500

    Improved/more responsive UI and removed unnecessary lines of code as well as fixed formatting issues

--- test.js
+++ test.js
@@ -1,23 +1,26 @@
-var canvas=document.getElementById("canvas");
-var context=canvas.getContext("2d");
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
 
-var cw=canvas.width;
-var ch=canvas.height;
-function reOffset(){
-  var BB=canvas.getBoundingClientRect();
-  offsetX=BB.left;
-  offsetY=BB.top;        
+var cw = canvas.width;
+var ch = canvas.height;
+var offsetX, offsetY;
+
+function reOffset() {
+  var BB = canvas.getBoundingClientRect();
+  offsetX = BB.left;
+  offsetY = BB.top;        
 }
-var offsetX,offsetY;
-reOffset();
-window.onscroll=function(e){ reOffset(); }
 
-context.lineWidth=2;
-context.strokeStyle='blue';
+reOffset();
+window.onscroll = function(e) { reOffset(); }
+
+context.lineWidth = 2;
+context.strokeStyle = 'blue';
 
 var coordinates = [];
-var isDone=false;
-var m = 0;
+var isDone = false;
+
+//button-clicking functionality
 $('#play').click(function() {
   alert("Play functionality must be implemented")
 })
@@ -25,31 +28,31 @@ $('#pause').click(function() {
   alert("Pause functionality must be implemented")
 })
 $('#done').click(function(){
-  isDone=true;
+  isDone = true;
 });
 
-$("#canvas").mousedown(function(e){handleMouseDown(e);});
+$("#canvas").mousedown(function(e) {handleMouseDown(e);});
 
-function handleMouseDown(e){
-  if(isDone || coordinates.length>10){return;}
+function handleMouseDown(e) {
+  if(isDone || coordinates.length > 10) {return;}
 
   // tell the browser we're handling this event
   e.preventDefault();
   e.stopPropagation();
 
-mouseX=parseInt(e.clientX-offsetX);
-mouseY=parseInt(e.clientY-offsetY);
-coordinates.push({x:mouseX,y:mouseY});
-drawPolygon();
+  mouseX = parseInt(e.clientX - offsetX);
+  mouseY = parseInt(e.clientY - offsetY);
+  coordinates.push({x:mouseX, y:mouseY});
+  drawPolygon();
 }
 
-function drawPolygon(){
-context.clearRect(0,0,cw,ch);
-context.beginPath();
-context.moveTo(coordinates[0].x, coordinates[0].y);
-for(index=1; index<coordinates.length;index++) {
-  context.lineTo(coordinates[index].x, coordinates[index].y);
-}
-context.closePath();
-context.stroke();
+function drawPolygon() {
+  context.clearRect(0,0,cw,ch);
+  context.beginPath();
+  context.moveTo(coordinates[0].x, coordinates[0].y);
+  for (index = 1; index<coordinates.length; index++) {
+    context.lineTo(coordinates[index].x, coordinates[index].y);
+  }
+  context.closePath();
+  context.stroke();
 }
