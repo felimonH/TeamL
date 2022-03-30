e2fd54e749443a8f10aa856f7eb816eb64ab3b7d
adamnguyen7 <adangu814@gmail.com>
2022-03-29 21:09:25 -0400

    New Unit Tests

--- Test2/src/Testing/footer/__test__/footer.test.js
+++ Test2/src/Testing/footer/__test__/footer.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Footer from './../footer';
+import Footer from '../footer';
 
 import { render } from "@testing-library/react"
-
+//strict unit test
 it('renders correctly', () => {
     const div = document.createElement("div")
     ReactDOM.render(<Footer/>, div)
